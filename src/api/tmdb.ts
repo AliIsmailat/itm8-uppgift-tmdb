9ca201d0a06b883437810c6306b1a692d16204b1
AliIsmailat <ali.ismailat@gritacademy.se>
2025-12-02 23:53:06 +0100

    Kommentarer + scroll funktion

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,11 +1,20 @@
-// src/api/tmdb.ts
+
+
+//importerar typen för movie och actor.
 import type { Movie, Actor } from "../types/types";
 
 
 
+//api key och base url:en här
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY as string;
 const BASE_URL = "https://api.themoviedb.org/3";
 
+
+//skapar funktionen fetchPopularMovies och exporterar den så att den kan
+//användas utanför filen.
+
+//fetchar popular movies, returneras som json och omvandlas till 
+//javascript objekt
 export const fetchPopularMovies = async (): Promise<Movie[]> => {
   const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
   if (!res.ok) {
@@ -17,30 +26,44 @@ export const fetchPopularMovies = async (): Promise<Movie[]> => {
 };
 
 
+//funktion för att söka filmer, tar in en query sträng och
+//försöker matcha query med filmerna, returnerar som data.results
 export const searchMovies = async (query: string): Promise<Movie[]> => {
   const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${encodeURIComponent(query)}`);
   const data = await res.json();
   return data.results;
 };
 
+
+//funktion för att hämta fetchmoviedetails, hämtar detaljer om filmen
+//med hjälp av ID på filmen, t.ex. 550
 export const fetchMovieDetails = async (id: number): Promise<Movie> => {
   const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`);
   const data = await res.json();
   return data;
 };
 
+
+//funktion för att hämta skådespelare (data.cast) för en viss film, sker också
+//med ID
 export const fetchMovieCast = async (id: number): Promise<Actor[]> => {
   const res = await fetch(`${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`);
   const data = await res.json();
   return data.cast; 
 };
 
+
+//funktion för att hämta detaljer om skådespelare för en film,
+//skådespelare har egna ID
 export const fetchActorDetails = async (id: number): Promise<Actor> => {
   const res = await fetch(`${BASE_URL}/person/${id}?api_key=${API_KEY}&language=en-US`);
   const data = await res.json();
   return data;
 };
 
+
+//hämta en skådespelares filmer, cast och crew returneras egentligen
+//dock tar vi bara emot cast.
 export const fetchActorMovies = async (id: number): Promise<Movie[]> => {
   const res = await fetch(`${BASE_URL}/person/${id}/movie_credits?api_key=${API_KEY}&language=en-US`);
   const data = await res.json();
--- src/pages/ActorDetails.tsx
+++ src/pages/ActorDetails.tsx
@@ -1,21 +1,49 @@
+
+//importerar useparams för att hämta parametrar från URL:en
+//i detta fallet id för skådespelaren vi vill visa.
+//link för att navigera utan att ladda omm hemsidan
+//navigate för (navigate-1) för tillbaka knappen
 import { useParams, Link, useNavigate } from "react-router-dom";
+
+//övriga importer
 import { useEffect, useState } from "react";
 import { fetchActorDetails, fetchActorMovies } from "../api/tmdb";
 import type { Actor, Movie } from "../types/types";
 import BackButton from "../components/BackButton";
 
+
+
+// funktion för att styra actordetails genom url parametrar
 export default function ActorDetails() {
   const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate(); // Lägg till navigate
+
+  //navigate använder usenavigate för backbutton
+  const navigate = useNavigate();
+
+  //usestate för actor, börjar som null, kan vara actor eller null
   const [actor, setActor] = useState<Actor | null>(null);
+
+//usestate för movies, börjar som en tom array
   const [movies, setMovies] = useState<Movie[]>([]);
+
+  //används för loading text, börjar som true, 
+  // sätts som false inom funktioner
   const [loading, setLoading] = useState(true);
 
+//useeffect för att hämta data, har id som dependecy, alltså körs on mount
+//och körs när id ändras
   useEffect(() => {
+
+    //om id är false avrbtyer vi funktionen
     if (!id) return;
+
+    //omvandlar id från URL:en till ett nr (vet ej varför?)
     const actorId = parseInt(id);
 
+    //kör två async anrop parallelt, fetchactordetails och fetchactormovies
     Promise.all([fetchActorDetails(actorId), fetchActorMovies(actorId)]).then(
+      // när det är klart, använder setactor, setmovies och setloading
+      // för att uppdatera states.
       ([actorData, movieData]) => {
         setActor(actorData);
         setMovies(movieData);
@@ -24,12 +52,15 @@ export default function ActorDetails() {
     );
   }, [id]);
 
+  //fallback + loading text
   if (loading) return <p className="text-center mt-10">Loading...</p>;
   if (!actor) return <p>Actor not found.</p>;
 
+
+
   return (
     <div className="p-4 max-w-5xl mx-auto">
-      {/* Back button */}
+      {/* knapp med onclick funktion, navigate(-1) går tillbaka */}
       <BackButton onClick={() => navigate(-1)} label="← Return to movie" />
 
       <div className="flex flex-col sm:flex-row gap-6 mb-6 mt-4">
