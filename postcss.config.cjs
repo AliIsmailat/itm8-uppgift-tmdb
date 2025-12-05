87d31dbe76c099aa7db155a2e66f8e00f48022c2
AliIsmailat <ali.ismailat@gritacademy.se>
2025-12-05 20:12:14 +0100

    tailwind fix (again)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,6 @@
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('@tailwindcss/postcss7-compat');
-
 module.exports = {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,8 @@
-// tailwind.config.cjs
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./Frontend/**/*.{js,ts,jsx,tsx,md}" 
+    "./Frontend/**/*.{js,ts,jsx,tsx,md}"
   ],
   theme: { extend: {} },
   plugins: [],
